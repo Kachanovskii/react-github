9ffbc8eb15bae5ed0f2bc30caadfa57cde1b4871
Kachanovskii <vadimskyi1@gmail.com>
2024-10-26 18:23:50 +0300

    finish poject

--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-import { ServerResponse, IUser } from "../../models/models"
+import { ServerResponse, IUser, IRepo } from "../../models/models"
 
 export const githubApi = createApi({
     reducerPath: 'github/api',
@@ -7,16 +7,22 @@ export const githubApi = createApi({
         baseUrl: 'https://api.github.com/'
     }),
     endpoints: build => ({
-        searchUsers: build.query<ServerResponse<IUser>, string>({
+        searchUsers: build.query<IUser[], string>({
             query: (search: string) => ({
                 url: `search/users`,
                 params: {
                     q: search,
                     per_page: 10
                 }
+            }),
+            transformResponse: (response: ServerResponse<IUser>) => response.items
+        }),
+        getUserRepos: build.query<IRepo[], string>({
+            query: (username: string) => ({
+                url: `users/${username}/repos`
             })
         })
     })
 })
 
-export const {useSearchUsersQuery} = githubApi
+export const {useSearchUsersQuery, useLazyGetUserReposQuery} = githubApi
